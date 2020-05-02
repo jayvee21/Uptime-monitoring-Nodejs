5c97a650eb53b3d815d524d80518594735b5ca3e
Jayvee <jaygerero21@gmail.com.com>
2020-05-02 15:31:03 +0800

    checkHandler

--- routers/checks.js
+++ routers/checks.js
@@ -6,11 +6,23 @@ let router = express.Router()
 var helpers = require('./../lib/helpers')
 var checksHandler = require('./../lib/checksHandler')
 var tokenVerifier = require('./../middleware/tokenVerifier')
+
+
 /**
  * Creating a new checks
  */
 router.post('/', tokenVerifier.checkToken, function( req, res ){
     checksHandler.create( req, function( statusCode, payload ){
+        helpers.handleServerResponse(res, statusCode, payload)
+    })
+})
+
+
+/**
+ * View checks
+ */
+router.get('/:checkId', tokenVerifier.checkToken, function(req, res){
+    checksHandler.get( req, function( statusCode, payload){
         helpers.handleServerResponse(res, statusCode, payload)
     })
 })
